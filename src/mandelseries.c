7e227af467959bffb5c11492e43d9767b783c7fb
addonovan <austin@addonovan.com>
2018-03-16 22:29:54 -0500

    Reformatted source files

--- src/mandelseries.c
+++ src/mandelseries.c
@@ -39,100 +39,129 @@ int main( int argc, char *argv[] )
   double xcenter = 0;
   double ycenter = 0;
   double scale = 4;
-  int    image_width = 500;
-  int    image_height = 500;
-  int    max = 1000;
+  int image_width = 500;
+  int image_height = 500;
+  int max = 1000;
 
   // For each command line argument given,
   // override the appropriate configuration value.
 
-  while((c = getopt(argc,argv,"x:y:s:W:H:m:o:h"))!=-1) {
-    switch(c) {
+  while( ( c = getopt( argc, argv, "x:y:s:W:H:m:o:h" ) ) != -1 ) 
+  {
+    switch( c )
+    {
       case 'x':
-        xcenter = atof(optarg);
+        xcenter = atof( optarg );
         break;
+
       case 'y':
-        ycenter = atof(optarg);
+        ycenter = atof( optarg );
         break;
+
       case 's':
-        scale = atof(optarg);
+        scale = atof( optarg );
         break;
+
       case 'W':
-        image_width = atoi(optarg);
+        image_width = atoi( optarg );
         break;
+
       case 'H':
-        image_height = atoi(optarg);
+        image_height = atoi( optarg );
         break;
+
       case 'm':
-        max = atoi(optarg);
+        max = atoi( optarg );
         break;
+
       case 'o':
         outfile = optarg;
         break;
+
       case 'h':
         show_help();
-        exit(1);
+        exit( 1 );
         break;
     }
   }
 
   // Display the configuration of the image.
-  printf("mandel: x=%lf y=%lf scale=%lf max=%d outfile=%s\n",xcenter,ycenter,scale,max,outfile);
+  printf( 
+      "mandel: x=%lf y=%lf scale=%lf max=%d outfile=%s\n", 
+      xcenter,
+      ycenter,
+      scale,
+      max,
+      outfile
+  );
 
   // Create a bitmap of the appropriate size.
-  struct bitmap *bm = bitmap_create(image_width,image_height);
+  struct bitmap *bm = bitmap_create( image_width,image_height );
 
   // Fill it with a dark blue, for debugging
-  bitmap_reset(bm,MAKE_RGBA(0,0,255,0));
+  bitmap_reset( bm, MAKE_RGBA( 0,0,255,0 ) );
 
   // Compute the Mandelbrot image
-  compute_image(bm,xcenter-scale,xcenter+scale,ycenter-scale,ycenter+scale,max);
+  compute_image( 
+      bm, 
+      xcenter - scale, 
+      xcenter + scale, 
+      ycenter - scale, 
+      ycenter + scale, 
+      max
+  );
 
   // Save the image in the stated file.
-  if(!bitmap_save(bm,outfile)) {
-    fprintf(stderr,"mandel: couldn't write to %s: %s\n",outfile,strerror(errno));
+  if( !bitmap_save( bm, outfile ) ) 
+  {
+    fprintf( 
+        stderr, 
+        "mandel: couldn't write to %s: %s\n", 
+        outfile, 
+        strerror( errno ) 
+    );
     return 1;
   }
 
   return 0;
 }
 
-/*
-Compute an entire Mandelbrot image, writing each point to the given bitmap.
-Scale the image to the range (xmin-xmax,ymin-ymax), limiting iterations to "max"
-*/
-
+/**
+ * Compute an entire Mandelbrot image, writing each point to the given bitmap.
+ * Scale the image to the range (xmin-xmax,ymin-ymax), limiting iterations to "max"
+ */
 void compute_image( struct bitmap *bm, double xmin, double xmax, double ymin, double ymax, int max )
 {
   int i,j;
 
-  int width = bitmap_width(bm);
-  int height = bitmap_height(bm);
+  int width = bitmap_width( bm );
+  int height = bitmap_height( bm );
 
   // For every pixel in the image...
 
-  for(j=0;j<height;j++) {
+  for( j = 0; j < height; j++ )
+  {
 
-    for(i=0;i<width;i++) {
+    for( i = 0; i < width; i++ )
+    {
 
       // Determine the point in x,y space for that pixel.
-      double x = xmin + i*(xmax-xmin)/width;
-      double y = ymin + j*(ymax-ymin)/height;
+      double x = xmin + i * ( xmax - xmin ) / width;
+      double y = ymin + j * ( ymax - ymin ) / height;
 
       // Compute the iterations at that point.
-      int iters = iterations_at_point(x,y,max);
+      int iters = iterations_at_point( x, y, max );
 
       // Set the pixel in the bitmap.
-      bitmap_set(bm,i,j,iters);
+      bitmap_set( bm, i, j, iters );
     }
   }
 }
 
-/*
-Return the number of iterations at point x, y
-in the Mandelbrot space, up to a maximum of max.
-*/
-
+/**
+ * Return the number of iterations at point x, y
+ * in the Mandelbrot space, up to a maximum of max.
+ */
 int iterations_at_point( double x, double y, int max )
 {
   double x0 = x;
@@ -142,8 +171,8 @@ int iterations_at_point( double x, double y, int max )
 
   while( (x*x + y*y <= 4) && iter < max ) {
 
-    double xt = x*x - y*y + x0;
-    double yt = 2*x*y + y0;
+    double xt = x * x - y * y + x0;
+    double yt = 2 * x * y + y0;
 
     x = xt;
     y = yt;
@@ -151,7 +180,7 @@ int iterations_at_point( double x, double y, int max )
     iter++;
   }
 
-  return iteration_to_color(iter,max);
+  return iteration_to_color( iter, max );
 }
 
 /*
@@ -162,8 +191,8 @@ Modify this function to make more interesting colors.
 
 int iteration_to_color( int i, int max )
 {
-  int gray = 255*i/max;
-  return MAKE_RGBA(gray,gray,gray,0);
+  int gray = 255 * i / max;
+  return MAKE_RGBA( gray, gray, gray, 0 );
 }
 
 
