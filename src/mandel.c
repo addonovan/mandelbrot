e6ec73c1dc92df8d092d3f38dadbcdf7a6fbdabe
addonovan <austin@addonovan.com>
2018-03-18 17:35:52 -0500

    Did all of the threaded version
    I really should've though to check this in more frequently

--- src/mandel.c
+++ src/mandel.c
@@ -8,26 +8,19 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+
+//
+// Definitions
+//
 
 typedef struct
 {
-  char *file_name;
-  double x_center;
-  double y_center;
-  double scale;
-  int image_width;
-  int image_height;
-  int max;
-  int process_count;
-}
-options_t;
-
-typedef struct 
-{
-  int pid;
-  char* file_name;
-  
   bitmap* bm;
+
+  int width;
+  int height;
   
   double x_min;
   double x_max;
@@ -37,16 +30,28 @@ typedef struct
 
   int max;
 }
-mandelbrot_t;
+image_params_t;
+
+typedef struct
+{
+  const image_params_t* params;
+
+  int row_start;
+  int row_end;
+}
+work_t;
+
+int work_pool_index = 0;
+work_t* work_pool = NULL;
+pthread_mutex_t m_work_pool = PTHREAD_MUTEX_INITIALIZER;
+
+//
+// Declarations
+//
 
 int iteration_to_color( int i, int max );
-
 int iterations_at_point( double x, double y, int max );
-
-void spawn_threads( options_t options );
-
-void mandelbrot_compute( mandelbrot_t* this );
-
+void* mandelbrot_compute( void* );
 void show_help();
 
 //
@@ -59,49 +64,53 @@ int main( int argc, char *argv[] )
 
   // These are the default configuration values used
   // if no command line arguments are given.
-  options_t options;
-  options.file_name = "mandel.bmp";
-  options.x_center = 0;
-  options.y_center = 0;
-  options.scale = 4;
-  options.image_width = 500;
-  options.image_height = 500;
-  options.max = 1000;
-  options.process_count = 1;
+  char* file_name = "mandel.bmp";
+  double x_center = 0;
+  double y_center = 0;
+  double scale = 4;
+  int image_width = 500;
+  int image_height = 500;
+  int max = 1000;
+  int thread_count = 1;
+  bool work_stealing = false;
 
   // For each command line argument given,
   // override the appropriate configuration value.
 
-  while( ( c = getopt( argc, argv, "x:y:s:W:H:m:o:h" ) ) != -1 ) 
+  while( ( c = getopt( argc, argv, "x:y:s:W:H:m:o:hw" ) ) != -1 ) 
   {
     switch( c )
     {
+      case 'w':
+        work_stealing = true;
+        break;
+
       case 'x':
-        options.x_center = atof( optarg );
+        x_center = atof( optarg );
         break;
 
       case 'y':
-        options.y_center = atof( optarg );
+        y_center = atof( optarg );
         break;
 
       case 's':
-        options.scale = atof( optarg );
+        scale = atof( optarg );
         break;
 
       case 'W':
-        options.image_width = atoi( optarg );
+        image_width = atoi( optarg );
         break;
 
       case 'H':
-        options.image_height = atoi( optarg );
+        image_height = atoi( optarg );
         break;
 
       case 'm':
-        options.max = atoi( optarg );
+        max = atoi( optarg );
         break;
 
       case 'o':
-        options.file_name = optarg;
+        file_name = optarg;
         break;
 
       case 'h':
@@ -111,148 +120,153 @@ int main( int argc, char *argv[] )
     }
   }
 
-  if ( argv[ optind ] == NULL ) 
+  int i;
+  for ( i = optind; i < argc; i++ )
   {
-    printf( "No process count specified\n\n" );
-    show_help();
-    exit( 1 );
-    return 1;
+    thread_count = atoi( argv[ i ] );
   }
-  options.process_count = atoi( argv[ optind ] );
 
   // Display the configuration of the image.
   printf( 
-      "mandel: x=%lf y=%lf scale=%lf max=%d outfile=%s\n", 
-      options.x_center,
-      options.y_center,
-      options.scale,
-      options.max,
-      options.file_name
+      "mandel: x=%lf y=%lf scale=%lf max=%d outfile=%s threads=%d %s\n", 
+      x_center,
+      y_center,
+      scale,
+      max,
+      file_name,
+      thread_count,
+      ( work_stealing ? "(work stealing)" : "" )
   );
 
-  // take everything before the extension in the file name, then create a new
-  // format string from that
-  const char* file_name = strtok( options.file_name, "." );
-  char* file_name_format = calloc( sizeof( char ), strlen( file_name ) + 7 );
-  sprintf( file_name_format, "%s%%d.bmp", file_name );
-  options.file_name = file_name_format;
-   
-  spawn_threads( options );
+  // create the generic params struct
+  image_params_t params = {
+    .x_min = x_center - scale,
+    .x_max = x_center + scale,
+    .y_min = y_center - scale,
+    .y_max = y_center + scale,
+    .bm = bitmap_create( image_width, image_height ),
+    .width = image_width,
+    .height = image_height,
+    .max = max
+  };
 
-  return 0;
-}
-
-void spawn_threads( options_t options )
-{
-  int remaining = 50;
-  int active = 0;
-  double scale = 2.0;
-  double step = ( 2.0 - options.scale ) / remaining;
-
-  mandelbrot_t mandel;
-  mandel.bm = bitmap_create( options.image_width, options.image_height );
-  bitmap_reset( mandel.bm, MAKE_RGBA( 0, 0, 255, 0 ) );
-  
-  mandel.max = options.max;
-  
-  for ( ; remaining > 0; remaining-- )
+  // determine the size of our work pool (depends on work stealing)
+  int work_size = thread_count;
+  if ( work_stealing )
   {
-    pid_t child = fork();
+    work_size = image_height;
+  }
 
-    // failed to fork
-    if ( child < 0 )
+  // create the work pool
+  pthread_mutex_init( &m_work_pool, NULL );
+  work_pool = malloc( sizeof( work_t ) * work_size );
+  work_pool_index = work_size - 1; // work from the back to the front
+
+  int start_row = 0;
+  for ( i = 0; i < work_size; i++ )
+  {
+    work_pool[ i ].params = &params;
+    work_pool[ i ].row_start = start_row;
+
+    start_row += ( image_height / work_size );
+    work_pool[ i ].row_end = start_row;
+  }
+
+  // make sure the entire image is generated
+  work_pool[ work_size - 1 ].row_end = image_height + 1;
+
+  // create the thread array
+  pthread_t* threads = malloc( sizeof( pthread_t ) * thread_count );
+  
+  // spawn off all of the threads
+  for ( i = 0; i < thread_count; i++ )
+  {
+    if ( pthread_create( threads + i, NULL, mandelbrot_compute, NULL ) )
     {
-      fprintf( stderr, "Failed to spawn process!\n" );
-      exit( 1 );
-    }
-    // we're in the child
-    else if ( child == 0 )
-    {
-      mandel.file_name = calloc( sizeof( char ), 2048 );
-      sprintf( mandel.file_name, options.file_name, remaining );
-
-      mandel.pid = remaining;
-      mandel.x_min = options.x_center - scale;
-      mandel.x_max = options.x_center + scale;
-      mandel.y_min = options.y_center - scale;
-      mandel.y_max = options.y_center + scale;
-      fflush( stdout );
-
-      mandelbrot_compute( &mandel );
-      exit( 0 );
-    }
-
-    printf( "[%d] Started %d\n", getpid(), remaining );
-    fflush( stdout );
-
-    // => we're in the parent, so continue dispatching new images
-    scale -= step;
-    active++;
-
-    // if we have too many running processes, then we're going to wait
-    // until we get down to the point where we can spawn another process
-    while ( active >= options.process_count )
-    {
-      wait( NULL );
-      active--;
+      perror( "Error creating thread: " );
+      exit( EXIT_FAILURE );
     }
   }
 
-  // wait for any stragglers
-  while ( wait( NULL ) > 0 );
+  // wait for all threads to finish
+  for ( i = 0; i < thread_count; i++ )
+  {
+    if ( pthread_join( threads[ i ], NULL ) )
+    {
+      perror( "Problem with pthread_join: " );
+    }
+  }
+
+  // write the final image
+  if( !bitmap_save( params.bm, file_name ) ) 
+  {
+    fprintf( 
+        stderr, 
+        "mandel: couldn't write to %s: %s\n",
+        file_name,
+        strerror( errno ) 
+    );
+    fflush( stderr );
+    fflush( stdout );
+
+    return 1;
+  }
+
+  return 0;
 }
 
 /**
  * Compute an entire Mandelbrot image, writing each point to the given bitmap.
  * Scale the image to the range (xmin-xmax,ymin-ymax), limiting iterations to "max"
  */
-void mandelbrot_compute( mandelbrot_t* this )
+void* mandelbrot_compute( void* _ )
 {
-  int i,j;
+  (void)(_);
 
-  int width = bitmap_width( this->bm );
-  int height = bitmap_height( this->bm );
+  const work_t* work = NULL;
 
-  // For every pixel in the image...
-
-  for( j = 0; j < height; j++ )
+  // each thread will stay alive until there's no more work for it to do
+  while ( work_pool_index >= 0 )
   {
+    // get the next work item from the pool
+    {
+      pthread_mutex_lock( &m_work_pool ); 
+      work = work_pool + work_pool_index;
+      work_pool_index--;
+      pthread_mutex_unlock( &m_work_pool );
+    }
 
-    for( i = 0; i < width; i++ )
+    int i, j;
+
+    const image_params_t* info = work->params;
+
+    int width = bitmap_width( info->bm );
+    int height = bitmap_height( info->bm );
+
+    // For every pixel in the image...
+
+    for( j = work->row_start; j < work->row_end; j++ )
     {
 
-      // Determine the point in x,y space for that pixel.
-      double x = this->x_min + i * ( this->x_max - this->x_min ) / width;
-      double y = this->y_min + j * ( this->y_max - this->y_min ) / height;
+      for( i = 0; i < width; i++ )
+      {
 
-      // Compute the iterations at that point.
-      int iters = iterations_at_point( x, y, this->max );
+        // Determine the point in x,y space for that pixel.
+        double x = info->x_min + i * ( info->x_max - info->x_min ) / width;
+        double y = info->y_min + j * ( info->y_max - info->y_min ) / height;
 
-      // Set the pixel in the bitmap.
-      bitmap_set( this->bm, i, j, iters );
+        // Compute the iterations at that point.
+        int iters = iterations_at_point( x, y, info->max );
+
+        // Set the pixel in the bitmap.
+        // This seems dangerous (modifying shared data), but it's guaranteed that
+        // we can't trample this memory because this row will only be edited by us
+        bitmap_set( info->bm, i, j, iters );
+      }
     }
   }
 
-  // Save the image in the stated file.
-  if( !bitmap_save( this->bm, this->file_name ) ) 
-  {
-    fprintf( 
-        stderr, 
-        "%d [%d] mandel: couldn't write to %s: %s\n",
-        this->pid,
-        getpid(),
-        this->file_name,
-        strerror( errno ) 
-    );
-    fflush( stderr );
-    fflush( stdout );
-
-    exit( 1 );
-    return;
-  }
-
-  printf( "[%d] Finished %d\n", getpid(), this->pid );
-  fflush( stdout );
+  return NULL;
 }
 
 /**
@@ -294,8 +308,8 @@ int iteration_to_color( int i, int max )
 
 void show_help()
 {
-  printf( "Use: mandel [options] <process_count>\n" );
-  printf( "Where process_count is the number of processes to run at one time\n" );
+  printf( "Use: mandel [options] <thread_count>\n" );
+  printf( "Where thread_count is the number of processes to run at one time\n" );
   printf( "\n" );
   printf( "Where options are:\n" );
   printf( "-m <max>    The maximum number of iterations per point. (default=1000)\n" );
